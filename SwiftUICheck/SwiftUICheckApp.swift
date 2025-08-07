9038a3373c05f7f747b68ede4341a2a4ae7ff8c0
shivam2018 <shivamtrivedi93@gmail.com>
2025-08-08 00:20:39 +0530

    Initial Commit

---
+++ SwiftUICheck/SwiftUICheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICheckApp.swift
+//  SwiftUICheck
+//
+//  Created by Shivam Trivedi on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUICheckTests/SwiftUICheckTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUICheckTests.swift
+//  SwiftUICheckTests
+//
+//  Created by Shivam Trivedi on 08/08/25.
+//
+
+import Testing
+
+struct SwiftUICheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
